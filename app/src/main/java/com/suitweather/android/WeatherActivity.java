8f890a11989ec5c5d7b5c5fef41313eabf5d364a
luojuntao <843024527@qq.com>
2019-03-06 19:37:12 +0800

    修改状态栏背景问问题

--- app/src/main/java/com/suitweather/android/WeatherActivity.java
+++ app/src/main/java/com/suitweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.suitweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -24,6 +25,7 @@ import com.bumptech.glide.Glide;
 import com.suitweather.android.gson.Forecast;
 import com.suitweather.android.gson.Lifestyle;
 import com.suitweather.android.gson.Weather;
+import com.suitweather.android.service.AutoUpdateService;
 import com.suitweather.android.util.HttpUtil;
 import com.suitweather.android.util.Utility;
 
@@ -68,8 +70,7 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_weather);
@@ -234,6 +235,8 @@ public class WeatherActivity extends AppCompatActivity {
             }
         }
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
     }
 }
